a4bc949a11620d4cb59de45f27763695b9e1c181
Johannes Stoppler <johannesstoppler@Johanness-MBP.attlocal.net>
2024-09-23 13:46:32 -0700

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+const apiKey = "";
+
+const currentSearchResult = document.getElementById("current-search-results");
+const futureForecast = document.getElementById("future-forecast");
+const futureForecastText = document.getElementById("future-forecast-text");
+const form = document.getElementById("search-form");
+const searchBtn = document.getElementById("search-btn");
+const pastSearches = document.getElementById("past-searches");
+
